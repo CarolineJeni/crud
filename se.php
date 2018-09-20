7446243ca812ef79e46ae144f435a63e3d9bebe5
Caroline <coolcaroline1996@gmail.com>
2018-09-20 09:48:30 +0530

    show details

---
+++ se.php
@@ -0,0 +1,63 @@
+<?php
+ require('db.php');
+?>
+<!DOCTYPE html>
+<html>
+<head>
+<style>
+#customers {
+    font-family: "Trebuchet MS", Arial, Helvetica, sans-serif;
+    border-collapse: collapse;
+    width: 100%;
+}
+
+#customers td, #customers th {
+    border: 1px solid #ddd;
+    padding: 8px;
+}
+
+#customers tr:nth-child(even){background-color: #f2f2f2;}
+
+#customers tr:hover {background-color: #ddd;}
+
+#customers th {
+    padding-top: 12px;
+    padding-bottom: 12px;
+    text-align: left;
+    background-color: #4CAF50;
+    color: white;
+}
+</style>
+<meta charset="utf-8">
+<title>View Records</title>
+<!--<link rel="stylesheet" href="css/style.css" />-->
+</head>
+<body>
+<div class="form">
+<p><a href="insert.php"><center>Insert New Record</center></a></p>
+<h2><center>View Records</center></h2>
+<table id="customers">
+<thead>
+<tr><th><strong>Name</strong></th><th><strong>Age</strong></th><th><strong>Edit</strong></th><th><strong>Delete</strong></th></tr>
+</thead>
+<tbody>
+<?php
+      $sel_query="Select * from new_record ORDER BY id desc;";
+      $result = mysqli_query($con,$sel_query);
+ 
+ while($row = mysqli_fetch_assoc($result)){
+	   ?>
+ <tr><td align="center"><?php echo $row["name"]; ?><td align="center"><?php echo $row["age"]; ?></td><td align="center"><a href="edit.php?id=<?php echo $row["id"]; ?>">Edit</a></td><td align="center"><a href="delete.php?id=<?php echo $row["id"]; ?>" onclick="return confirm('Sure to Delete?'); ">Delete</a></td></tr>
+<script type="text/javascript">
+   function delete_id(id){
+         if(confirm('Sure To Delete ?')){
+         window.location.href='view.php?delete_id='+id;
+     }
+}
+</script>
+<?php  } ?>
+</tbody>
+</table>
+</div>
+</body>
+</html>
